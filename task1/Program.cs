d44df0900a2705df138b1b00a57e52c60cab0ec3
Vladislav_Grudzinsky <vladik19780131@yandex.ru>
2024-04-18 02:21:53 +0300

    Задача 1. Решена.

--- task1/Program.cs
+++ task1/Program.cs
@@ -6,3 +6,39 @@
 // M = 4; N = 8 -> "4, 5, 6, 7, 8"
 // M = 8; N = 4 -> "8, 7, 6, 5, 4"
 
+void PrintNaturalNumbersLH(int numstart, int numend)
+{
+    int num = numend;
+    if (num < numstart)
+    {
+        return;
+    }
+    PrintNaturalNumbersLH(numstart, numend - 1);
+    Console.Write($"{num}, ");
+}
+
+void PrintNaturalNumbersHL(int numstart, int numend)
+{
+    int num = numstart;
+    if (num > numend)
+    {
+        return;
+    }
+    PrintNaturalNumbersHL(numstart + 1, numend);
+    Console.Write($"{num}, ");
+}
+
+Console.WriteLine("Введите натуральное число — начало желаемого диапазона всех натуральных чисел: ");
+int naturalnumberstart = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите натуральное число — конец желаемого диапазона всех натуральных чисел: ");
+int naturalnumberend = Convert.ToInt32(Console.ReadLine());
+
+if (naturalnumberstart < naturalnumberend)
+{
+    PrintNaturalNumbersLH(naturalnumberstart, naturalnumberend);    
+}
+else
+{
+    PrintNaturalNumbersHL(naturalnumberend, naturalnumberstart);
+}
