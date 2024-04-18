212733be415f3527f585c1259f0e64304a39d5ea
Vladislav_Grudzinsky <vladik19780131@yandex.ru>
2024-04-18 07:19:18 +0300

    Задача 3 решена

--- task3/Program.cs
+++ task3/Program.cs
@@ -3,3 +3,49 @@
 //Пример:
 // [1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1
 
+int[] CreateArrayRndInt(int count, int min, int max)
+{
+    int[] array = new int[count];
+    Random rnd = new Random();
+    for (int i = 0; i < count; i++)
+    {
+        array[i] = rnd.Next(min, max);
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write("]");
+}
+
+
+void PrintFlipArray(int[] array, int first = 0)
+{
+    if (first != array.Length)
+    {
+        PrintFlipArray(array, first + 1);
+        Console.Write($"{array[first]}, ");
+    }
+}
+
+
+Console.WriteLine("Введите количество элементов будущего массива из чисел: ");
+int countelem = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите наименьшее число в будущем произвольном массиве: ");
+int minnum = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите наибольшее число в будущем произвольном массиве: ");
+int maxnum = Convert.ToInt32(Console.ReadLine());
+
+int[] arrayrnd = CreateArrayRndInt(countelem, minnum, maxnum);
+PrintArray(arrayrnd);
+Console.WriteLine("\n Развёрнутый массив: ");
+PrintFlipArray(arrayrnd);
